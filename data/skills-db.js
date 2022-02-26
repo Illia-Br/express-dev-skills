c6d8e2b76e26334d2c1358126c58a815418e148f
Illia Brytun <elbrytun@gmail.com>
2022-02-26 17:35:43 -0500

    add skill-db

---
+++ data/skills-db.js
@@ -0,0 +1,5 @@
+const skills = [
+  {technology: 'HTML', level: 'beginner', _id: 111222},
+  {technology: 'CSS', level: 'intermediate', _id: 121835},
+  {technology: 'JavaScript', level: 'expert', _id: 436119},
+]
